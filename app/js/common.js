94a8c7c35e7177bc49dc1cc96eea714dbfe20c13
panviktar <panviktar.gmail.com>
2018-07-09 16:54:55 +0300

    mobile mnu

--- app/js/common.js
+++ app/js/common.js
@@ -16,6 +16,12 @@ $(function() {
 		});
 	});
 
+	// $(".toggle-mnu").click(function() {
+	// 	$(this).toggleClass("on");
+	// 	$(".main-mnu").slideToggle();
+	// 	return false;
+	// });
+
 
 
 	// Custom JS
